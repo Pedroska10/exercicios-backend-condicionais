c25971608e73d7078c4b8a8ece250fe2deee9b3e
Pedroska10 <pedro.phsr2002@gmail.com>
2024-12-01 14:51:56 -0300

    perf: implementação ternário

--- 02/index.js
+++ 02/index.js
@@ -1,8 +1,4 @@
 const jogada1 = 5;
-const jogada2 = 3;
+const jogada2 = 4;
 
-if ((jogada1 + jogada2) % 2 === 0) {
-    console.log('Par')
-} else {
-    console.log('ímpar')
-}
+console.log((jogada1 + jogada2) % 2 === 0 ? 'par' : 'impar')
