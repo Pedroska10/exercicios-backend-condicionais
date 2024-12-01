4eb04e41ab10ab81feeccc8ccdd56dcf5ea073f6
Pedroska10 <pedro.phsr2002@gmail.com>
2024-12-01 16:35:34 -0300

    perf: otimização lógica

--- 04/index.js
+++ 04/index.js
@@ -1,18 +1,14 @@
 const ladoA = 6;
 const ladoB = 6;
+const numeroDaBucha = ladoA
+const nomeDaBucha = {
+    0: 'Branco',
+    1: 'Ás',
+    2: 'Duque',
+    3: 'Terno',
+    4: 'Quadra',
+    5: 'Quina',
+    6: 'Sena'
+};
 
-if (ladoA && ladoB === 0) {
-    console.log('Branco')
-} else if (ladoA && ladoB === 1) {
-    console.log('Ás')
-} else if (ladoA && ladoB === 2) {
-    console.log('Duque')
-} else if (ladoA && ladoB === 3) {
-    console.log('Terno')
-} else if (ladoA && ladoB === 4) {
-    console.log('Quadra')
-} else if (ladoA && ladoB === 5) {
-    console.log('Quina')
-} else if (ladoA && ladoB === 6) {
-    console.log('Sena')
-}
+console.log(`${nomeDaBucha[numeroDaBucha]}`)
